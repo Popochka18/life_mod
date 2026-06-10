bd46c1dd0a9a37a60724d51ea45abc46c231916f
popochka18 <rakamik@yandex.ru>
2026-06-10 17:35:26 +0300

    Initialisation of template

---
+++ src/client/kotlin/com/lifemod/client/LifeModClient.kt
@@ -0,0 +1,9 @@
+package com.lifemod.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object LifeModClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/kotlin/com/lifemod/LifeMod.kt
@@ -0,0 +1,15 @@
+package com.lifemod
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object LifeMod : ModInitializer {
+    private val logger = LoggerFactory.getLogger("life-mod")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
